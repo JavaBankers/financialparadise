b31546cb8133703d3033a7d37ee8d0e4450943e7
Grzegorz <cagaragrzegorz@gmail.com>
2017-11-28 18:31:10 +0100

    add customer added

--- src/main/java/com/sda/spring/controller/CustomerController.java
+++ src/main/java/com/sda/spring/controller/CustomerController.java
@@ -1,9 +1,17 @@
 package com.sda.spring.controller;
 
+import com.sda.spring.entity.Customer;
 import com.sda.spring.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
@@ -14,5 +22,16 @@ public class CustomerController {
     @RequestMapping("/")
     public String mainPage(){
         return "mainPage";
+    }
+
+    @PostMapping("/addCustomer")
+    public String addCustomer(@ModelAttribute Customer customer, ModelMap modelMap){
+        customerService.addCustomer(customer);
+        return "success";
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+        return customerService.getAllCustomers();
     }
 }
