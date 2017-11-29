1136949e9300a5d1b8ff551a18beeb30d811a966
Rafal Ciesla <ra.ciesla@wp.pl>
2017-11-29 20:07:26 +0100

    fixed connection issues

--- src/main/java/com/sda/spring/FinparadiseApplication.java
+++ src/main/java/com/sda/spring/FinparadiseApplication.java
@@ -1,5 +1,7 @@
 package com.sda.spring;
 
+import com.sda.spring.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +9,9 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FinparadiseApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	private AccountService accountService;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
