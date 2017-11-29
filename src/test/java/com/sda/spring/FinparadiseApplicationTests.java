0c89d42bf0ca9d0da75cb761c172e3ddcc8f2df5
Grzegorz <cagaragrzegorz@gmail.com>
2017-11-29 19:46:16 +0100

    properties refactor

--- src/test/java/com/sda/spring/FinparadiseApplicationTests.java
+++ src/test/java/com/sda/spring/FinparadiseApplicationTests.java
@@ -10,9 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FinparadiseApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 	@Test
 	public void testCreateTables() {
