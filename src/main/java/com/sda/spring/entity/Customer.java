f922cbff44c466b61ae9ee8402e025105822429e
Grzegorz <cagaragrzegorz@gmail.com>
2017-11-30 18:05:27 +0100

    validation works

--- src/main/java/com/sda/spring/entity/Customer.java
+++ src/main/java/com/sda/spring/entity/Customer.java
@@ -17,8 +17,6 @@ public class Customer {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-//    @PeselValidator
-//    @Size(min = 11)
     private String pesel;
     private String email;
     private String password;
