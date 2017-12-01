c1a10ac0f737eff8b99aa65c819a54a90681cd0e
Filip Bielicki <filip.lk.bielicki@gmail.com>
2017-12-01 20:53:38 +0100

    Implemented new functionality in SuspiciousTransferChecker & tested returning biggest transaction from last 12 months

--- src/main/java/com/sda/spring/repository/TransferHistoryRepository.java
+++ src/main/java/com/sda/spring/repository/TransferHistoryRepository.java
@@ -3,6 +3,10 @@ package com.sda.spring.repository;
 import com.sda.spring.entity.TransferHistory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TransferHistoryRepository extends CrudRepository<TransferHistory, Integer> {
+
+    List<TransferHistory> findTransferHistoriesByBankAccountNumberFrom(String bankAccountNumber);
 
 }
---
+++ src/test/java/com/sda/spring/SuspiciousTransferCheckerTest.java
@@ -0,0 +1,69 @@
+package com.sda.spring;
+
+import com.sda.spring.entity.TransferHistory;
+import com.sda.spring.repository.SuspiciousPersonRepository;
+import com.sda.spring.service.TransferHistoryService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class SuspiciousTransferCheckerTest {
+
+    private SuspiciousTransferChecker transferChecker;
+
+    @Mock
+    private SuspiciousPersonRepository mockedSuspiciousPersonRepository;
+
+    @Mock
+    private TransferHistoryService mockedTransferHistoryService;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        transferChecker = new SuspiciousTransferChecker(mockedSuspiciousPersonRepository, mockedTransferHistoryService);
+    }
+
+    @Test
+    public void shouldReturnBiggestTransaction() throws Exception {
+
+        when(mockedTransferHistoryService.getTransferHistoryForSpecificAccount("123")).thenReturn(getTransferHistoriesList());
+
+        BigDecimal result = transferChecker.getBiggestTransferFromLast12Moths("123");
+
+        assertThat(result).isEqualTo(new BigDecimal("855.22"));
+        verify(mockedTransferHistoryService, times(1)).getTransferHistoryForSpecificAccount("123");
+    }
+
+    @Test
+    public void shouldReturnTransferHistoriesList() throws Exception {
+
+
+
+    }
+
+    private List<TransferHistory> getTransferHistoriesList() {
+        List<TransferHistory> histories = new LinkedList<>();
+        histories.add(new TransferHistory("123", "456",
+                LocalDateTime.of(2017, 11, 28, 22, 33), new BigDecimal("355.00")));
+        histories.add(new TransferHistory("123", "456",
+                LocalDateTime.of(2017, 10, 28, 22, 33), new BigDecimal("855.22")));
+        histories.add(new TransferHistory("123", "456",
+                LocalDateTime.of(2017, 11, 28, 22, 33), new BigDecimal("145.37")));
+        return histories;
+    }
+}
